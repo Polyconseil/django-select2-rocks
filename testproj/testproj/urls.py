88753c259fe6d73d9619316a2a499632fa4ac322
Alexandre Muller <alexandre.muller@polyconseil.fr>
2023-12-19 17:21:36 +0100

    testproj: replace url by re_path
    alias deprecated in Django 3.1
    
    https://docs.djangoproject.com/en/5.0/releases/3.1/#id2

--- testproj/testproj/urls.py
+++ testproj/testproj/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 
 from testproj.testapp.views import BeachList, index, json_beaches
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^json/$', json_beaches, name='json_beaches'),
-    url(r'^restframework/$', BeachList.as_view(), name='rest_beach_list'),
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^$', index, name='index'),
+    re_path(r'^json/$', json_beaches, name='json_beaches'),
+    re_path(r'^restframework/$', BeachList.as_view(), name='rest_beach_list'),
+    re_path(r'^admin/', admin.site.urls),
 ]
